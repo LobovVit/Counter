a91054b5442ff6c349870a8e32f954f3548fa5b7
vitaly <lobov.vitaliy@gmail.com>
2024-07-13 14:25:40 +0300

    Changing buttons behavior

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -20,14 +20,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        incrementButton.backgroundColor = .systemGreen
-        decrementButton.backgroundColor = .systemRed
-        resetButton.backgroundColor = .systemBlue
         historyTextView.text = historyText
         scoreLabel.text = String(score)
+        buttonChange(btn: incrementButton,btnColor: UIColor.systemGreen, borderColor: UIColor.gray.cgColor)
+        buttonChange(btn: decrementButton,btnColor: UIColor.systemRed, borderColor: UIColor.gray.cgColor)
+        buttonChange(btn: resetButton,btnColor: UIColor.systemBlue, borderColor: UIColor.gray.cgColor)
     }
 
+    private func buttonChange(btn: UIButton, btnColor: UIColor,  borderColor: CGColor) {
+        btn.layer.cornerRadius = 15
+        btn.layer.borderWidth = 3
+        btn.layer.borderColor = borderColor
+        btn.backgroundColor = btnColor
+        
+    }
+    
     @IBAction func decrementTouch(_ sender: Any) {
+        buttonChange(btn: decrementButton,btnColor: UIColor.systemRed, borderColor: UIColor.gray.cgColor)
         if score > 0 {
             score -= 1
             logger(message: "значение изменено на \(score) (-1)")
@@ -39,12 +48,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func incrementTouch(_ sender: Any) {
+        buttonChange(btn: incrementButton,btnColor: UIColor.systemGreen, borderColor: UIColor.gray.cgColor)
         score += 1
         logger(message: "значение изменено на \(score) (+1)")
         show()
     }
     
     @IBAction func resetTouch(_ sender: Any) {
+        buttonChange(btn: resetButton,btnColor: UIColor.systemBlue, borderColor: UIColor.gray.cgColor)
         score = 0
         logger(message: "значение сброшено")
         show()
@@ -62,5 +73,14 @@ class ViewController: UIViewController {
         historyText = "\(DateFormatter.string(from: date)): \(message)" + "\n" + historyText
     }
     
+    @IBAction func decrementTouchDown(_ sender: Any) {
+        buttonChange(btn: decrementButton,btnColor: UIColor.gray, borderColor: UIColor.systemRed.cgColor)
+    }
+    @IBAction func IncrementTouchDown(_ sender: Any) {
+        buttonChange(btn: incrementButton,btnColor: UIColor.gray, borderColor: UIColor.systemGreen.cgColor)
+    }
+    @IBAction func resetTouchDown(_ sender: Any) {
+        buttonChange(btn: resetButton,btnColor: UIColor.gray, borderColor: UIColor.systemBlue.cgColor)
+    }
 }
 
